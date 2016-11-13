76051caa0d3386c4934e2fca6699a7b4bbe1a583
Richard Farman <farmanrl@whitman.edu>
2016-11-12 16:14:39 -0800

    a

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { initAuth } from './redux/auth';
+import configureStore from './redux/store';
+import Root from './components/Root';
+
+const store = configureStore();
+const syncedHistory = syncHistoryWithStore(browserHistory, store);
+const rootElement = document.getElementById('root');
+
+
+function render(Root) {
+  ReactDOM.render(
+    <Root history={syncedHistory} store={store} />,
+    rootElement
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('./components/Root', () => {
+    render(require('./components/Root').default);
+  });
+}
+
+initAuth(store.dispatch)
+  .then(() => render(Root))
+  .catch(error => console.error(error)); // eslint-disable-line no-console
